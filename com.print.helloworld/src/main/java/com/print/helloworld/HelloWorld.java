37a83974e7a645416d410a12706dbd34e9d48d0d
Somonnoy <34779582+Somonnoy@users.noreply.github.com>
2021-05-30 10:38:42 +0530

    2nd commit

--- com.print.helloworld/src/main/java/com/print/helloworld/HelloWorld.java
+++ com.print.helloworld/src/main/java/com/print/helloworld/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 System.out.println("Hello World");
 System.out.println("Hello World 20210530");
-
+System.out.println("Hello World something new");
 	}
 
 }
